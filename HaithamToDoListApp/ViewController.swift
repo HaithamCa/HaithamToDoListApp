39ba53a7416c7379e6549bea1ea8ce3849e60958
hytham defrawy <hythamdefrawy@gmail.com>
2023-11-22 14:38:45 -0500

    Added new more three screens with its functions

--- HaithamToDoListApp/ViewController.swift
+++ HaithamToDoListApp/ViewController.swift
@@ -5,8 +5,8 @@
 //  Created by hytham defrawy on 2023-11-20.
 //
 
-import RealmSwift
 import UIKit
+import RealmSwift
 
 
 class ToDoListItem: Object {
@@ -15,38 +15,66 @@ class ToDoListItem: Object {
 }
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+
     @IBOutlet var table: UITableView!
-    
-    // Create an array for the Todolist items
+
+    private let realm = try! Realm()
     private var data = [ToDoListItem]()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        data = realm.objects(ToDoListItem.self).map({ $0 })
         table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         table.delegate = self
         table.dataSource = self
     }
 
+    // Mark: Table
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-            cell.textLabel?.text = data[indexPath.row].item
-            return cell
-        }
-    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row].item
+        return cell
+    }
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        //open the screen where we can see item info and delete
+
+        // Open the screen where we can see item info and dleete
+        let item = data[indexPath.row]
+
+        guard let vc = storyboard?.instantiateViewController(identifier: "view") as? ViewViewController else {
+            return
+        }
+
+        vc.item = item
+        vc.deletionHandler = { [weak self] in
+            self?.refresh()
+        }
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.title = item.item
+        navigationController?.pushViewController(vc, animated: true)
     }
 
     @IBAction func didTapAddButton() {
-        
+        guard let vc = storyboard?.instantiateViewController(identifier: "enter") as? EntryViewController else {
+            return
+        }
+        vc.completionHandler = { [weak self] in
+            self?.refresh()
+        }
+        vc.title = "New Item"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
     }
-    
-}
 
+    func refresh() {
+        data = realm.objects(ToDoListItem.self).map({ $0 })
+        table.reloadData()
+    }
+
+}
